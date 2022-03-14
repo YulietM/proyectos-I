f3bdbe35d5a99774302c01c0b8e9ef3db7c0a59b
Duvan Vargas <dvargas@conexia.com>
2022-03-13 21:39:36 -0500

    Se agregan nuevas funciones y sus casos de prueba

--- basics/tarea-1/funciones-matematicas.js
+++ basics/tarea-1/funciones-matematicas.js
@@ -1,26 +1,66 @@
-// module "funciones-matematicas.js"
-
 /**
  * Funciones matematicas
- */
-const funcionesMat = {
-	/**
-	 *
-	 * @param {NumeroEntero} numero
-	 * @returns
-	 */
-	factorial: (numero) => {
-		return;
-	},
-	/**
-	 *
-	 * @param {lista de numeros, ejemplo [1,2,3]} listaNumeros
-	 * @returns
-	 */
-	sumatoria: (listaNumeros) => {
+*/
+const funcionesMat = {};
+export default funcionesMat;
 
-		return listaNumeros;
-	}
+/**
+ * Funcion que calcual el factorial de un numero dado<br>
+ * Ejemplo: si el numero =4, el factorial es 1+2+3+4=10
+ *
+ * @param {number} numero
+ * @returns
+ */
+funcionesMat.factorial = (numero) => {
+	//TODO: Logica para calcular el factorial de un numero
+	return numero;
+};
+/**
+ * Funcion que calcula la sumatoria de una lista de numeros dada<br>
+ * Ejemplo: si listaNumeros=[4,7,2,6] entonces 4+7+2+6=19 el valor retornado es  19,
+ *
+ * @param {Array} listaNumeros
+ * @returns
+ */
+funcionesMat.sumatoria = (listaNumeros) => {
+	//TODO: logica para calcular la sumatoria de listaNUmeros
+	return listaNumeros;
 };
 
-export default funcionesMat;
+
+/**
+ * Funcion que retorna el numero mayor de la lista dada
+ *
+ * @param {Array} listaNumeros
+ * @returns  numero mayor de la lista;
+ */
+funcionesMat.numeroMayor = (listaNumeros) => {
+	//TODO: Logica para encontrar el numero mayor de la lista de numeros
+	return listaNumeros;
+};
+
+
+
+/**
+ * Funcion que retorna el numero mayor de la lista dada
+ *
+ * @param {Array} listaNumeros
+ * @returns  numero mayor de la lista;
+ */
+funcionesMat.numeroMayor = (listaNumeros) => {
+	//TODO: Logica para encontrar el numero mayor de la lista de numeros
+	return listaNumeros;
+};
+
+/**
+ * Funcion que dadas dos listas de numeros
+ * retorna el numero menor que se encuentran tanto la lista 1 como la lista 2
+ * @param {Array, Array} lista1, lista2
+ */
+funcionesMat.numeroMenorComun = (lista1, lista2) => {
+	//TODO: Logica para encontrar el numero menor que esta tanto en la lista1 uno como en la lista2
+	return [];
+}
+
+
+
--- spec/funciones-matematicas.spec.js
+++ spec/funciones-matematicas.spec.js
@@ -3,33 +3,75 @@ import funcionesMat from '../basics/tarea-1/funciones-matematicas.js';
 /**
  * Prubas de la funcion factorial
  */
-describe('Prueba funcionesMat.factorial: ', () => {
-	/**
-	 * Factorial correcto
-	 */
-	it('la funcion factorial debe calcular el factorial del numero dado', () => {
-		let result = funcionesMat.factorial(4);
-		expect(result).toBe(10);
-	});
-
-	/**
-	 * numero recibido
-	 */
-	it('el valor recibido debe ser entero, para otro tipo de valores debe retornar undefined', () => {
-		let result = funcionesMat.factorial(4.5);
-		expect(result).toBe(undefined);
+describe('funcionesMat.factorial() ', () => {
+	//given
+	let tests = [
+		{ args: 4, expected: 10 },
+		{ args: 7, expected: 28 },
+		{ args: 9, expected: 45 },
+		{ args: 0, expected: 0 }
+	];
+	//when
+	tests.forEach(test => {
+		it('la funcion factorial debe calcular el factorial del numero dado ', () => {
+			let result = funcionesMat.factorial(test.args);
+			expect(result).toBe(test.expected);
+		});
 	});
 
 });
 
-describe("Prueba funcionesMat.sumatoria ", () => {
+describe("funcionesMat.sumatoria() ", () => {
 
-	it('la funcion sumatoria debe calcular la sumatoria de una lista de numeros', () => {
-		let result = funcionesMat.sumatoria([4, 5, 7, 4, 7, 9]);
-		expect(result).toBe(36);
+	//given
+	let tests = [
+		{ args: [2, 5, 6, 7, 8], expected: 28 },
+		{ args: [1, 4, 5, 6], expected: 16 },
+		{ args: [], expected: 0 }
+	];
+	//when
+	tests.forEach(test => {
+		it('la funcion sumatoria debe calcular la sumatoria de una lista de numeros', () => {
+			let result = funcionesMat.sumatoria(test.args);
+			expect(result).toBe(test.expected);
+		});
 	});
 
+});
 
+describe("funcionesMatTestCase() ", () => {
 
-})
+	//given
+	let suites = [
+		{
+			callFunction: funcionesMat.numeroMayor,
+			mensaje: 'la funcion numeroMayor() debe retornar el numero mayor una lista de numeros',
+			tests: [
+				{ args: [[2, 5, 6, 7, 8]], expected: 8 },
+				{ args: [[1, 4, 22, 5, 6]], expected: 22 },
+				{ args: [], expected: 0 }
+			]
+		},
+		{
+			callFunction: funcionesMat.numeroMenorComun,
+			mensaje: 'la funcion numeroMenorComun() debe retornar el numero menor que esta en ambas listas',
+			tests: [
+				{ args: [[2, 5, 6, 7, 8], [2, 5, 6, 7, 8]], expected: 2 },
+				{ args: [[0, 4, 22, 5, 6], [0, 4, 22, 5, 6]], expected: 0 },
+				{ args: [[4, 22, 5, 6], [4, 22, 5, 6]], expected: 4 }
+			]
+		},
+
+	];
+	//when
+	suites.forEach(funcionATestear => {
+		funcionATestear.tests.forEach(caso => {
+			it('=>' + funcionATestear.callFunction.name + ' ' + funcionATestear.mensaje, () => {
+				let result = funcionATestear.callFunction.apply(funcionATestear.callFunction, Array.prototype.slice.call(caso.args, 0));
+				expect(result).toBe(caso.expected);
+			});
+		});
+	});
+
+});
 
